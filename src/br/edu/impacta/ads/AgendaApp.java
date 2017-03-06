f22b4b59df5f5585e545d02e7dff2871d638c30a
Renata Oliveira <renatabels@gmail.com>
2017-03-06 09:42:21 -0300

    Encapsulamento das operações de acesso aos contatos

--- src/br/edu/impacta/ads/AgendaApp.java
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 
 public class AgendaApp {
 	private static Scanner entrada = new Scanner(System.in);
-	private static List<Contato> contatos = new ArrayList<>();
+	//private static List<Contato> contatos = new ArrayList<>();
+	private static IContatoDao dao = new ContatoDao();
+	
 	public static void main(String[] args) {
 		boolean sair = false;
 		while (!sair) {
@@ -49,10 +51,12 @@ public class AgendaApp {
 		String nome = lerNome();
 		String telefone = lerTelefone();
 		Contato c = new Contato(nome, telefone);
-		if(contatos.contains(c)) {
+		//if(contatos.contains(c)) {
+		if(dao.existe(c)) {
 			System.out.println("Este contato já está cadastrado!");
 		}else{
-			contatos.add(c);
+			//contatos.add(c);
+			dao.inserir(c);
 			System.out.println("Contato inserido!");
 		}
 	}
@@ -92,12 +96,15 @@ public class AgendaApp {
 	private static void buscarContato() {
 		System.out.println("\n BUSCA DE CONTATOS: ");
 		String nome = lerNome();
-		List<Contato> resultado = new ArrayList<>();
-		for (Contato c : contatos) {
-			if (nome.equals(c.getNome())) {
-				resultado.add(c);
-			}
-		}
+//		List<Contato> resultado = new ArrayList<>();
+//		for (Contato c : contatos) {
+//			if (nome.equals(c.getNome())) {
+//				resultado.add(c);
+//			}
+//		}
+		
+		List<Contato> resultado = dao.buscar(nome);
+		
 		if(resultado.size() == 0) {
 			System.out.println("Não há contato com esse nome!");
 		} else {
