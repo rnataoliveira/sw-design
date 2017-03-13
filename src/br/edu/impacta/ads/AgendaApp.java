f26eeaf3d94530e992650213630c18a4019f4391
Renata Oliveira <renatabels@gmail.com>
2017-03-13 09:10:50 -0300

    Persistência dos dados em arquivos.

--- src/br/edu/impacta/ads/AgendaApp.java
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -1,13 +1,12 @@
 package br.edu.impacta.ads;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class AgendaApp {
 	private static Scanner entrada = new Scanner(System.in);
 	//private static List<Contato> contatos = new ArrayList<>();
-	private static IContatoDao dao = new ContatoDao();
+	private static IContatoDao dao = new ContaDaoArquivo("contato.txt");
 	
 	public static void main(String[] args) {
 		boolean sair = false;
